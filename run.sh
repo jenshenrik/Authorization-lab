da1b232c6e546484c84ea6fe56e448c6cc366065
Jens Henrik Skuldbøl <jenshenrik.skuldbol@gmail.com>
2013-11-27 15:32:16 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+#java -classpath .:bin/:SampleAzn.jar:SampleAction.jar:SampleLM.jar -Djava.security.manager -Djava.security.policy==sampleazn.policy -Djava.security.auth.login.config==sample_jaas.config sample.SampleAzn
+
+java -classpath SampleAzn.jar:SampleAction.jar:SampleLM.jar -Djava.security.manager -Djava.security.policy==sampleazn.policy -Djava.security.auth.login.config==sample_jaas.config sample.SampleAzn
