fdfe3656d81acf3275f3e316146aa2ea6c177b86
Jens Henrik Skuldbøl <jenshenrik.skuldbol@gmail.com>
2013-11-27 15:49:55 +0100

    Fixed error in build file

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 echo "Building..."
-javac sample/*.java sample/module/*.java sample/principal/*.java
+javac sample/SampleAzn.java sample/SampleAction.java sample/module/*.java sample/principal/*.java
 echo "Done.\n\nPackaging .jars..."
 echo "SampleAzn.jar:"
 jar -cvf SampleAzn.jar sample/SampleAzn.class sample/MyCallbackHandler.class
 echo "SampleAction.jar:"
-jar -cvf SampleAction sample/SampleAction.class
+jar -cvf SampleAction.jar sample/SampleAction.class
 echo "SampleLM.jar:"
 jar -cvf SampleLM.jar sample/module/SampleLoginModule.class sample/principal/SamplePrincipal.class
 echo "Done.\n\nAll done."
